2a90d7c385de3afcace8cdf4a207fe60a8eca687
Kyle <kyle.lanier.mscs@gmail.com>
2020-04-28 23:40:50 -0500

    Updated docstring

--- modules/docstrings/user_interface.py
+++ modules/docstrings/user_interface.py
@@ -11,32 +11,31 @@ for interactions with the pysqlite database
 def ui():
     :Example:
 
-    >>>from modules.user_interface import ui
-    >>>ui()
-    >>>---------WSUAL DB---------
-    >>>[0]: Exit
-    >>>[1]: Execute Statement
-    >>>[2]: Print Table
+    >>> ui()
+    >>> ---------WSUAL DB---------
+    >>> [0]: Exit
+    >>> [1]: Execute Statement
+    >>> [2]: Print Table
     >>> 1
-    >>>Please enter a statement: SELECT * FROM Students WHERE studentName='Kyle'
-    >>>[('y839k364', 'Kyle', 'CS', 'Graduate', '2021-05-25', 1, 'WSU Wichita')]
-    >>>
-    >>>
-    >>>---------WSUAL DB---------
-    >>>[0]: Exit
-    >>>[1]: Execute Statement
-    >>>[2]: Print Table
+    >>> Please enter a statement: SELECT * FROM Students WHERE studentName='Kyle'
+    >>> [('y839k364', 'Kyle', 'CS', 'Graduate', '2021-05-25', 1, 'WSU Wichita')]
+    >>> 
+    >>> 
+    >>> ---------WSUAL DB---------
+    >>> [0]: Exit
+    >>> [1]: Execute Statement
+    >>> [2]: Print Table
     >>> 2
-    >>>
-    >>>Pleasse select a table:
-    >>>[0]: Students
-    >>>[1]: SkillSets
-    >>>[2]: Skills
-    >>>[3]: Projects
-    >>>[4]: Contracts
-    >>>[5]: Companies
-    >>>[6]: Locations
-    >>>[7]: Contains
+    >>> 
+    >>> Pleasse select a table:
+    >>> [0]: Students
+    >>> [1]: SkillSets
+    >>> [2]: Skills
+    >>> [3]: Projects
+    >>> [4]: Contracts
+    >>> [5]: Companies
+    >>> [6]: Locations
+    >>> [7]: Contains
     >>> 2
     skillName VARCHAR(50)    skillLevel VARCHAR(30)   description VARCHAR(50)
     Python                   High                     CS Programming
