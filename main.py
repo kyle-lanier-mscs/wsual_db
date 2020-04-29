c3d73916c4790229a6ac2d067ed8e97afa43d708
Kyle <kyle.lanier.mscs@gmail.com>
2020-04-28 19:43:32 -0500

    Updated documentation for Sphinx

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
 """
-Tutorial: https://repl.it/talk/learn/How-to-create-an-SQLite3-database-in-Python-3/15755
+File: main.py
+Date: 4.27.2020
+Author: Kyle Lanier
+
+Porpose:
+This file is used to serve as the main U.I.
+for interactions with the pysqlite database
+
+    :Example:
+
+    >>>main()
+
+    ---------WSUAL DB---------
+    [0]: Exit
+    [1]: Execute Statement
+    [2]: Print Table
+    > 1
+    Please enter a statement: SELECT * FROM Students WHERE studentName='Kyle'
+    [('y839k364', 'Kyle', 'CS', 'Graduate', '2021-05-25', 1, 'WSU Wichita')]
+    
+    
+    ---------WSUAL DB---------
+    [0]: Exit
+    [1]: Execute Statement
+    [2]: Print Table
+    > 2
+    
+    Pleasse select a table:
+    [0]: Students
+    [1]: SkillSets
+    [2]: Skills
+    [3]: Projects
+    [4]: Contracts
+    [5]: Companies
+    [6]: Locations
+    [7]: Contains
+    > 2
+    skillName VARCHAR(50)    skillLevel VARCHAR(30)   description VARCHAR(50)
+    Python                   High                     CS Programming
+    Power BI                 Medium                   Data Analytics
+    AWS                      Low                      Cloud Services
+    Office 365               Medium                   Microsoft Applications
+    Technical Support        Medium                   Support Services
+    Azure                    Low                      Cloud Services
+    C#                       Low                      CS Programming
+    Customer Service         High                     Support Services
+    MySQL                    Low                      Data Analytics
+
 """
 from modules.user_interface import ui
 
